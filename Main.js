bd9a981aa790c9de0f3019787f619d8e75867991
KitChaos <97907903+KitChaos-DreamscapeStudios@users.noreply.github.com>
2023-01-27 09:33:53 -0500

    Added Basic Buildings

--- Main.js
+++ Main.js
@@ -7,14 +7,30 @@ class Modifier{
     }
 
 }
-class Player{
+class PlayerData{
+   
     constructor(){
         this.Buildings = [];
         this.Modifers = [];
+        this.FoodCap = 20000
+        this.Food = 0
 
     }
 }
-const MainPlayer = Player();
+class Building{
+    constructor(Type, Production, Cost){
+        this.Type = Type
+        this.Production = Production
+        this.Cost = Cost
+
+    }
+}
+function Second(t){
+    return t*1000
+}
+const KibbleWorm = new Building("Kibble Worm", 0.01, 10)//For Production, take amount per second and divide by 1000
+const MainPlayer = new PlayerData();
+
 function Tick(){
     for(let i = 0; i < MainPlayer.Modifers.length; i++){
         if(MainPlayer.Modifers[i].TimeLeft != -100){
@@ -29,5 +45,23 @@ function Tick(){
        
         
     }
+    for(let i = 0; i<MainPlayer.Buildings.length; i++){
+        MainPlayer.Food += MainPlayer.Buildings[i].Production;
+    }
+    document.getElementById("FoodCounter").innerHTML = `Food: ${Math.round(MainPlayer.Food)}`
     window.setTimeout(Tick, 1)
+}
+function BuyBuilding(Building){
+    if(MainPlayer.Food >= Building.Cost){
+        MainPlayer.Buildings.push(Building)
+        MainPlayer.Food -= Building.Cost
+    }
+    else{
+        alert(`Not Enough Food to purchase ${Building.Type}`)
+    }
+   
+}
+function Click(){
+    MainPlayer.Food += 1
+    //Lets introduce more complex clicking logic later.
 }
